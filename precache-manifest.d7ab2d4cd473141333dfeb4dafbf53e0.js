43184f177e675f60b3c60be834814c25f987863d
rexbean <rexbean36@gmail.com>
2019-02-28 13:10:08 -0800

    Updates

---
+++ precache-manifest.d7ab2d4cd473141333dfeb4dafbf53e0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6512360f16ab3813e81a",
+    "url": "/mBlog/static/js/runtime~main.6512360f.js"
+  },
+  {
+    "revision": "c2e0871d1719daefcab8",
+    "url": "/mBlog/static/js/main.c2e0871d.chunk.js"
+  },
+  {
+    "revision": "e2bd485bd50bc5c1e3e4",
+    "url": "/mBlog/static/js/2.e2bd485b.chunk.js"
+  },
+  {
+    "revision": "c2e0871d1719daefcab8",
+    "url": "/mBlog/static/css/main.6ea31ab6.chunk.css"
+  },
+  {
+    "revision": "b047ec0f3fbfed2ad778cf8fffbd97e0",
+    "url": "/mBlog/index.html"
+  }
+];
